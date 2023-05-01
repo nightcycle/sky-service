75c2649904b309819a05b21cc7223777332b9e7c
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2023-05-01 16:53:49 -0600

    initial release

---
+++ src/Test.story.lua
@@ -0,0 +1,33 @@
+--!strict
+--Services
+local RunService = game:GetService("RunService")
+--Packages
+local Package = script.Parent
+assert(Package)
+local Packages = Package.Parent
+assert(Packages)
+local Maid = require(Packages:WaitForChild("Maid"))
+--Modules
+--Types
+type Maid = Maid.Maid
+--Constants
+local DAY_DURATION = 30
+
+return function(coreGui: Frame)
+	local maid = Maid.new()
+
+	task.spawn(function()
+		local SkyService = require(script.Parent)
+		SkyService.init(maid)
+		SkyService.Clouds:Set(0.5)
+		maid:GiveTask(RunService.RenderStepped:Connect(function(deltaTime: number)
+			local tVal = 0.5 + 0.5 * math.sin(tick() / DAY_DURATION)
+			print("RAIN", tVal)
+			SkyService.Rain:Set(tVal)
+		end))
+	end)
+
+	return function()
+		maid:Destroy()
+	end
+end
